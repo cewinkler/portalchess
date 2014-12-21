a9cfa17738e9ebbdb778dcbdf15418c671508939
Carl <carl@carlossus.com>
2014-12-21 15:30:37 +0800

    Adding angular modules and dependencies

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,5 @@
+﻿var appRoot = angular.module('main', ['ngRoute', 'ngResource', 'angularStart.services', 'angularStar.directives']);
+
+appRoot.config(['$routeProvider'], function($routeProvider) {
+    $routeProvider.when('/chess', { templateUrl: '/chess/index', controller: 'ChessController' });
+});
