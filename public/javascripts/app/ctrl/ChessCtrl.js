69b5f764f982e0b2db26a75e317b42badc704185
Carl <carl@carlossus.com>
2015-01-18 22:53:07 +0800

    Adding ng chess controller

---
+++ public/javascripts/app/ctrl/ChessCtrl.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ChessCtrl', ChessCtrl);
+
+    ChessCtrl.$inject = ['$scope']; 
+
+    function ChessCtrl($scope) {
+        $scope.title = 'ChessCtrl';
+
+        activate();
+
+        function activate() { }
+    }
+})();
