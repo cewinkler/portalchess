26946cdccb11b7cca4bde34a45d12358f063c48e
Carl <carl@carlossus.com>
2015-01-19 10:00:07 +0800

    Adding knockout models to project

---
+++ public/javascripts/ko/chess-board.ts
@@ -0,0 +1,22 @@
+﻿enum Player { White, Black };
+class ChessBoard {
+     whiteId: string;
+     blackId: string;
+     moveList: ChessMove[];
+ }
+
+ class ChessPosition {
+     file: number;
+     rank: number;
+     piece: IChessPiece;
+     private owner: Player;
+ }
+
+class ChessMove {
+    fromPosition: ChessPosition;
+    toPosition: ChessPosition;
+}
+
+ interface IChessPiece {
+     
+ }
